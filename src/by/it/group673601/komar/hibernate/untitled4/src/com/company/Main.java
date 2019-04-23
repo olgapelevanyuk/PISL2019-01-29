364f542498e25506ec5f18c8c6962fd46f8212da
Yulia <comar.yul@yandex.by>
2019-04-23 13:18:28 +0300

    hib

---
+++ src/by/it/group673601/komar/hibernate/untitled4/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package by.it.group673601.komar.hibernate.untitled4.src.com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println();
+    }
+}
